bf3d17a92a6254030cb7364b9e256f2eec35ecb4
root <root@DESKTOP-E81SO17.localdomain>
2021-02-09 12:22:37 -0800

    added Users,Sessions,And Tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    has_many :tweets
+    
+
+    validates :username, presence: true, length: { minimum: 3, maximum: 64 }
+    validates :email, presence: true, length: { minimum: 5, maximum: 500 }
+    validates :password, presence: true, length: {minimum: 8, maximum: 64 }
+    validates :term, presence: true, acceptance: {message: "Please Agree On Our Policy"}
+    validates_uniqueness_of :username
+    validates_uniqueness_of :email
+
+    # After Validation
+    after_validation :hash_password
+
+    private 
+        def hash_password
+            self.password = BCrypt::Password.create(self.password)
+        end
+
+    
 end
---
+++ db/migrate/20210209194728_add_atribute_to_user.rb
@@ -0,0 +1,5 @@
+class AddAtributeToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :term, :boolean
+  end
+end
